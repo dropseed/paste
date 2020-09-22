07470dfd9d0dd8ffeb37c47a10d0cf14b52b5b39
Dave Gaeddert <dave.gaeddert@gmail.com>
2020-09-22 10:56:32 -0500

    Create django_email_backend.py

---
+++ django_email_backend.py
@@ -0,0 +1,16 @@
+# Can save as authentication.py, and make it the first item in settings AUTHENTICATION_BACKENDS
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        try:
+            user = UserModel.objects.get(email__iexact=username)
+        except UserModel.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
